404790c6a1320b3b299ee0e11a090a6bb5b789e3
Ivan Deka <48100591+ivand17@users.noreply.github.com>
2020-06-20 12:11:55 +0530

    Initial commit

---
+++ src/Pathfinder/Node/Node.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+import "./Node.css";
+
+export default class Node extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const {
+      col,
+      isStart,
+      isFinish,
+      isWall,
+      onMouseDown,
+      onMouseEnter,
+      onMouseUp,
+      row
+    } = this.props;
+    const extraClassName = isFinish
+      ? "node-finish"
+      : isStart
+      ? "node-start"
+      : isWall
+      ? "node-wall"
+      : "";
+
+    return (
+      <div
+        id={`node-${row}-${col}`}
+        className={`node ${extraClassName}`}
+        onMouseDown={() => onMouseDown(row, col)}
+        onMouseEnter={() => onMouseEnter(row, col)}
+        onMouseUp={() => onMouseUp()}
+      />
+    );
+  }
+}
+
+export const DEFAULT_NODE = {
+  row: 0,
+  col: 0
+};
---
+++ src/algorithms/bfs.js
@@ -0,0 +1,1 @@
+export function bfs(grid, startNode, finishNode) {}
